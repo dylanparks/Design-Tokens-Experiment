fd9e3c1cf699f41a372ee0142aa9286f157fc22f
Dylan Parks <37780558+dylanparks@users.noreply.github.com>
2022-05-22 13:11:54 -0400

    Initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Title = styled.button`
+  color: #000;
+  font-family: 'Times New Roman';
+  font-weight: 600;
+  font-size: 3rem;
+`;
+
+export { Title };
