940dd4280f0b9cf7eb1d5bd76a39245543886647
Dylan Parks <37780558+dylanparks@users.noreply.github.com>
2022-05-22 13:20:41 -0400

    added folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./styles.css";
-import {Title} from "./components/Button.js";
+import { Title } from "./components/Button.js";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Title>Example</Title>
+      <Title>Example New</Title>
     </div>
   );
 }
